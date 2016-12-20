79fdc0ad3b0e4d515eb34d87f33db25cd6620031
lucassantosf <lucas46em@gmail.com>
2016-12-19 22:53:31 -0200

    Criar ListView

--- ListView/app/src/main/java/listview/cursoandroid/com/listview/MainActivity.java
+++ ListView/app/src/main/java/listview/cursoandroid/com/listview/MainActivity.java
@@ -3,8 +3,11 @@ package listview.cursoandroid.com.listview;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -32,5 +35,15 @@ public class MainActivity extends Activity {
         );
 
         listaItens.setAdapter( adaptador );
+
+        listaItens.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                int codigoPosicao = position;
+                String valorClicado = listaItens.getItemAtPosition(codigoPosicao).toString();
+                Toast.makeText(getApplicationContext(),valorClicado, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
