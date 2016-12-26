a6a2281ade29aac14c8208014233ba1da5fef4de
Lucas Ferreira <lukin_fs@hotmail.com>
2016-12-26 20:06:24 -0200

    Uso de ToggleButton, RadioButton

---
+++ ToggleButton/app/src/main/java/togglebutton/cursoandroid/com/togglebutton/MainActivity.java
@@ -0,0 +1,33 @@
+package togglebutton.cursoandroid.com.togglebutton;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class MainActivity extends Activity {
+
+    private ToggleButton toggleButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toggleButton = (ToggleButton) findViewById(R.id.toggleButtonId);
+
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if(isChecked){
+                    Toast.makeText(getApplicationContext(),"Ligado",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"Desligado",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
