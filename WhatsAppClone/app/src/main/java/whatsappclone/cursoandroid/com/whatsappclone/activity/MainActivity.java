3f133dc58911bc9d00185011034287e74f0a30e1
Lucas Ferreira <lukin_fs@hotmail.com>
2017-02-09 19:50:35 -0200

    Criação Toolbar e Logoff do usuário]

--- WhatsAppClone/app/src/main/java/whatsappclone/cursoandroid/com/whatsappclone/activity/MainActivity.java
+++ WhatsAppClone/app/src/main/java/whatsappclone/cursoandroid/com/whatsappclone/activity/MainActivity.java
@@ -3,6 +3,10 @@ package whatsappclone.cursoandroid.com.whatsappclone.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,25 +23,52 @@ public class MainActivity extends AppCompatActivity {
 
     private Button botao_sair;
     private FirebaseAuth autenticacao;
+    private Toolbar toolbar;
+    private FirebaseAuth firebase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        botao_sair = (Button) findViewById(R.id.bt_logout);
+        firebase = ConfiguracaoFirebase.getFirebaseAutenticacao();
 
-        botao_sair.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // recuperar a referencia do Firebase
-                autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
-                autenticacao.signOut();
-                // após sair, voltar para tela inicial
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("WhatsApp");
+        setSupportActionBar(toolbar);
+    }
 
-            }
-        });
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.item_sair:
+                deslogarUsuario();
+                return true;
+            case R.id.item_configuracoes:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    public void deslogarUsuario(){
+
+        firebase.signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+
     }
 }
