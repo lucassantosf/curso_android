d10e39ae628da6d9a7b2b89bed4ecdba72d815a3
Lucas Ferreira <lukin_fs@hotmail.com>
2017-04-22 20:15:55 -0300

    Nova Interface de Cadastro

--- WhatsAppClone/app/src/main/java/com/whatsappandroid/cursoandroid/whatsappclone/activity/CadastroUsuarioActivity.java
+++ WhatsAppClone/app/src/main/java/com/whatsappandroid/cursoandroid/whatsappclone/activity/CadastroUsuarioActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import com.whatsappandroid.cursoandroid.whatsappclone.R;
 
-public class ValidadorActivity extends AppCompatActivity {
+public class CadastroUsuarioActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_validador);
+        setContentView(R.layout.activity_cadastro_usuario);
     }
 }
--- WhatsAppClone/app/src/main/java/com/whatsappandroid/cursoandroid/whatsappclone/activity/LoginActivity.java
+++ WhatsAppClone/app/src/main/java/com/whatsappandroid/cursoandroid/whatsappclone/activity/LoginActivity.java
@@ -2,6 +2,7 @@ package com.whatsappandroid.cursoandroid.whatsappclone.activity;
 
 import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +12,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.github.rtoshiro.util.format.MaskFormatter;
 import com.github.rtoshiro.util.format.SimpleMaskFormatter;
@@ -24,117 +26,19 @@ import java.util.Random;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText telefone;
-    private EditText cod_pais;
-    private EditText cod_area;
-    private EditText nome;
-    private Button cadastrar;
-    private String[] permissoesNecessarias = new String[]{
-            Manifest.permission.SEND_SMS,
-            Manifest.permission.INTERNET
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Permissao.validaPermissoes(1, this, permissoesNecessarias);
 
-        telefone    = (EditText) findViewById(R.id.edit_telefone);
-        cod_pais    = (EditText) findViewById(R.id.edit_cod_pais);
-        cod_area    = (EditText) findViewById(R.id.edit_area);
-        nome        = (EditText) findViewById(R.id.edit_nome);
-        cadastrar   = (Button) findViewById(R.id.bt_cadastrar);
-
-        /* Definir mascaras */
-        SimpleMaskFormatter simpleMaskTelefone = new SimpleMaskFormatter("NNNNN-NNNN");
-        SimpleMaskFormatter simpleMaskCodArea = new SimpleMaskFormatter("NN");
-        SimpleMaskFormatter simpleMaskCodPais = new SimpleMaskFormatter("+NN");
-
-
-        MaskTextWatcher maskCodPais = new MaskTextWatcher(cod_pais, simpleMaskCodPais);
-        MaskTextWatcher maskCodArea = new MaskTextWatcher(cod_area, simpleMaskCodArea);
-        MaskTextWatcher maskTelefone = new MaskTextWatcher(telefone, simpleMaskTelefone);
-
-        cod_pais.addTextChangedListener( maskCodPais );
-        cod_area.addTextChangedListener( maskCodArea );
-        telefone.addTextChangedListener( maskTelefone );
-
-        cadastrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String nomeUsuario = nome.getText().toString();
-                String telefoneCompleto =
-                        cod_pais.getText().toString() +
-                        cod_area.getText().toString() +
-                        telefone.getText().toString();
-                String telefoneSemFormatacao = telefoneCompleto.replace("+","");
-                telefoneSemFormatacao = telefoneSemFormatacao.replace("-","");
-
-                //GERAR token
-                Random randomico = new Random();
-                int numeroRandomico = randomico.nextInt(9999 - 1000 ) + 1000;
-                String token = String.valueOf(numeroRandomico);
-                String mensagemEnvio = "WhatsApp Código de Confirmação: "+ token;
-
-                //Salvar dados para validacao
-                Preferencias preferencias = new Preferencias(LoginActivity.this);
-                preferencias.salvarUsuarioPreferencias(nomeUsuario, telefoneSemFormatacao, token);
-
-                //Envio do SMS
-                telefoneSemFormatacao = "5554";
-                boolean enviadoSMS = enviaSMS("+" + telefoneSemFormatacao, mensagemEnvio);
-
-                /*HashMap<String, String> usuario = preferencias.getDadosUsuario();
-                */
-
-
-            }
-        });
-    }
-    //envio de SMS
-    private boolean enviaSMS(String telefone, String mensagem){
-        try {
-
-            SmsManager smsManager = SmsManager.getDefault();
-            smsManager.sendTextMessage(telefone, null, mensagem, null, null);
-            return true;
-
-        }catch (Exception e){
-            e.printStackTrace();
-            return false;
-        }
 
     }
 
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+    public void abrirCadastroUsuario(View view){
 
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        for(int resultado : grantResults ){
+        Intent intent = new Intent(LoginActivity.this, CadastroUsuarioActivity.class);
+        startActivity(intent);
 
-            if( resultado == PackageManager.PERMISSION_DENIED){
-                alertaValidacaoPermissao();
-            }
-        }
-    }
-
-
-    private void alertaValidacaoPermissao(){
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Permissões");
-        builder.setMessage("Para utilizar este app, é necessário aceitas as permissões");
-
-        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                finish();
-            }
-        });
-
-        AlertDialog dialog = builder.create();
-        dialog.show();
     }
 }
