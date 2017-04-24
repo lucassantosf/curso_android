3a92ba52739e3b291f054a1673ce6cbd5d236362
Lucas Ferreira <lukin_fs@hotmail.com>
2017-04-24 15:55:16 -0300

    Criação da Toolbar

--- WhatsAppClone/app/src/main/java/com/whatsappandroid/cursoandroid/whatsappclone/activity/MainActivity.java
+++ WhatsAppClone/app/src/main/java/com/whatsappandroid/cursoandroid/whatsappclone/activity/MainActivity.java
@@ -3,6 +3,9 @@ package com.whatsappandroid.cursoandroid.whatsappclone.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,29 +17,27 @@ import com.whatsappandroid.cursoandroid.whatsappclone.config.ConfiguracaoFirebas
 
 public class MainActivity extends AppCompatActivity {
 
+    //private Firebase firebase;
+    
     private Button botaoSair;
-    private FirebaseAuth autenticacao;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        botaoSair = (Button) findViewById(R.id.bt_sair);
+        DatabaseReference firebase = ConfiguracaoFirebase.getFirebase();
 
-        botaoSair.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("WhatsApp");
+        setSupportActionBar(toolbar);
+    }
 
-                autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
-                autenticacao.signOut();
-
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-
-
-            }
-        });
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
