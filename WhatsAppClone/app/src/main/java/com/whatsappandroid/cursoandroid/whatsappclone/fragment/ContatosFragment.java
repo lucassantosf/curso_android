99a301d31180811368b9b26d019f35c86762dcc9
Lucas Ferreira <lukin_fs@hotmail.com>
2017-05-02 17:54:56 -0300

    Listando Contatos part 1

--- WhatsAppClone/app/src/main/java/com/whatsappandroid/cursoandroid/whatsappclone/fragment/ContatosFragment.java
+++ WhatsAppClone/app/src/main/java/com/whatsappandroid/cursoandroid/whatsappclone/fragment/ContatosFragment.java
@@ -6,14 +6,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.whatsappandroid.cursoandroid.whatsappclone.R;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ContatosFragment extends Fragment {
 
+    private ListView listView;
+    private ArrayAdapter adapter;
+    private ArrayList<String> contatos;
 
     public ContatosFragment() {
         // Required empty public constructor
@@ -23,8 +30,23 @@ public class ContatosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        //Instanciar Objetos
+        contatos = new ArrayList<>();
+
+        contatos.add("Lucas");
+        contatos.add("Yara");
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contatos, container, false);
+        View view =  inflater.inflate(R.layout.fragment_contatos, container, false);
+        listView = (ListView) view.findViewById(R.id.lv_contatos);
+        adapter = new ArrayAdapter(
+                getActivity(),
+                R.layout.lista_contato,
+                contatos
+        );
+        listView.setAdapter(adapter);
+        return view;
+
     }
 
 }
