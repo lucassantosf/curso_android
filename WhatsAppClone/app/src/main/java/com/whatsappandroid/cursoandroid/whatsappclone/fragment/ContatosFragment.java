7dc1f7aa84420ed063bd26aafa72abc5cdbc889d
Lucas Ferreira <lukin_fs@hotmail.com>
2017-05-03 15:53:24 -0300

    Listagem de contatos

--- WhatsAppClone/app/src/main/java/com/whatsappandroid/cursoandroid/whatsappclone/fragment/ContatosFragment.java
+++ WhatsAppClone/app/src/main/java/com/whatsappandroid/cursoandroid/whatsappclone/fragment/ContatosFragment.java
@@ -9,7 +9,14 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.whatsappandroid.cursoandroid.whatsappclone.R;
+import com.whatsappandroid.cursoandroid.whatsappclone.config.ConfiguracaoFirebase;
+import com.whatsappandroid.cursoandroid.whatsappclone.helper.Preferencias;
+import com.whatsappandroid.cursoandroid.whatsappclone.model.Contato;
 
 import java.util.ArrayList;
 
@@ -21,6 +28,7 @@ public class ContatosFragment extends Fragment {
     private ListView listView;
     private ArrayAdapter adapter;
     private ArrayList<String> contatos;
+    private DatabaseReference firebase;
 
     public ContatosFragment() {
         // Required empty public constructor
@@ -33,9 +41,6 @@ public class ContatosFragment extends Fragment {
         //Instanciar Objetos
         contatos = new ArrayList<>();
 
-        contatos.add("Lucas");
-        contatos.add("Yara");
-
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_contatos, container, false);
         listView = (ListView) view.findViewById(R.id.lv_contatos);
@@ -45,6 +50,36 @@ public class ContatosFragment extends Fragment {
                 contatos
         );
         listView.setAdapter(adapter);
+
+        //Recuperar contatos do firebase
+        Preferencias preferencias = new Preferencias(getActivity());
+        String identificadorUsuarioLogado = preferencias.getIdentificador();
+
+        firebase = ConfiguracaoFirebase.getFirebase()
+                .child("contatos")
+                .child(identificadorUsuarioLogado);
+
+        //Listener para recuperar contatos
+        firebase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //limpar a lista
+                contatos.clear();
+
+                //listar contatos
+                for(DataSnapshot dados : dataSnapshot.getChildren()){
+                    Contato contato = dados.getValue(Contato.class);
+                    contatos.add( contato.getNome() );
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
         return view;
 
     }
