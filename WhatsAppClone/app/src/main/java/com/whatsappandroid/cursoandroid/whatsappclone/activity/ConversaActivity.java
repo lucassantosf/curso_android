ce0b10af8ea217c0f690ab74cd4e4f821fc412f3
Lucas Ferreira <lukin_fs@hotmail.com>
2017-05-10 16:25:29 -0300

    Salvando mensagens enviadas no Firebase

--- WhatsAppClone/app/src/main/java/com/whatsappandroid/cursoandroid/whatsappclone/activity/ConversaActivity.java
+++ WhatsAppClone/app/src/main/java/com/whatsappandroid/cursoandroid/whatsappclone/activity/ConversaActivity.java
@@ -3,15 +3,31 @@ package com.whatsappandroid.cursoandroid.whatsappclone.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
 import com.whatsappandroid.cursoandroid.whatsappclone.R;
+import com.whatsappandroid.cursoandroid.whatsappclone.config.ConfiguracaoFirebase;
+import com.whatsappandroid.cursoandroid.whatsappclone.helper.Base64Custom;
+import com.whatsappandroid.cursoandroid.whatsappclone.helper.Preferencias;
+import com.whatsappandroid.cursoandroid.whatsappclone.model.Mensagem;
 
 public class ConversaActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private EditText editMensagem;
+    private ImageButton btMensagem;
+    private DatabaseReference firebase;
 
     // dados do destinatario
     private String nomeUsuarioDestinatario;
+    private String idUsuarioDestinatario;
+
+    //dados do remetente
+    private String idUsuarioRemetente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +35,62 @@ public class ConversaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_conversa);
 
         toolbar = (Toolbar) findViewById(R.id.tb_conversa);
+        editMensagem = (EditText) findViewById(R.id.edit_mensagem);
+        btMensagem = (ImageButton) findViewById(R.id.bt_enviar);
+
+        // recuperar dados do usuario logado
+        Preferencias preferencias = new Preferencias(ConversaActivity.this);
+        idUsuarioRemetente = preferencias.getIdentificador();
 
         Bundle extra = getIntent().getExtras();
 
         if( extra != null){
             nomeUsuarioDestinatario = extra.getString("nome");
+            String emailDestinatario = extra.getString("email");
+            idUsuarioDestinatario = Base64Custom.codificarBase64(emailDestinatario);
         }
 
         //Configurar toolbar
         toolbar.setTitle(nomeUsuarioDestinatario);
         toolbar.setNavigationIcon(R.drawable.ic_action_arrow_left);
         setSupportActionBar(toolbar);
+
+        //Eviar mensagem
+        btMensagem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String textoMensagem = editMensagem.getText().toString();
+
+                if(textoMensagem.isEmpty()){
+                    Toast.makeText(ConversaActivity.this, "Digite uma mensagem para enviar ", Toast.LENGTH_LONG).show();
+                }else{
+
+                    Mensagem mensagem = new Mensagem();
+                    mensagem.setIdUsuario(idUsuarioRemetente);
+                    mensagem.setMensagem( textoMensagem );
+                    salvarMensagem(idUsuarioRemetente, idUsuarioDestinatario, mensagem );
+                    editMensagem.setText("");
+                }
+            }
+        });
+
+    }
+
+    private boolean salvarMensagem(String idRemetente, String idDestinatario, Mensagem mensagem){
+        try{
+            firebase = ConfiguracaoFirebase.getFirebase().child("mensagens");
+
+            firebase.child(idRemetente)
+                    .child(idDestinatario)
+                    .push()
+                    .setValue(mensagem);
+            // push - cria identificador unico
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
 
