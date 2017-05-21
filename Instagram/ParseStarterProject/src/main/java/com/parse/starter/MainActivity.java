27d14e1c52a847d0e3739a752745256bf310aa1e
Lucas Ferreira <lukin_fs@hotmail.com>
2017-05-20 23:01:20 -0300

    Filtrando dados

--- Instagram/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ Instagram/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -15,12 +15,15 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.parse.FindCallback;
 import com.parse.GetCallback;
 import com.parse.ParseAnalytics;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.SaveCallback;
+
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -31,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
 
-
+    /*
       ParseObject pontuacao = new ParseObject("Pontuacao");
       pontuacao.put("nome","Tiana");
       pontuacao.put("pontos", 888);
@@ -48,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
           }
       });
 
-
+*/
       /*
       ParseQuery<ParseObject> consulta = ParseQuery.getQuery("Pontuacao");
       consulta.getInBackground("qzhwW1ISgH", new GetCallback<ParseObject>() {
@@ -67,6 +70,35 @@ public class MainActivity extends AppCompatActivity {
           }
       });
         */
+
+      ParseQuery<ParseObject> filtro = ParseQuery.getQuery("Pontuacao");
+
+      //Aplciando filtros na listagem de dados
+      filtro.whereGreaterThan("pontos", 800);
+      //filtro.whereGreaterThanOrEqualTo("pontos", 800);
+      //filtro.whereLessThan("pontos", 700);
+      //filtro.whereEndsWith("nome", "fer");
+      //filtro.whereStartsWith("nome", "lu");
+      filtro.addAscendingOrder("pontos");
+      filtro.setLimit(3);
+
+      //listar os dados
+      filtro.findInBackground(new FindCallback<ParseObject>() {
+          @Override
+          public void done(List<ParseObject> objects, ParseException e) {
+
+              if(e == null){// efetuada a lsitagem sem erros
+
+                  for( ParseObject object : objects){
+                      Log.i("ListarDados", "Objetos - " + object.get("nome") + " pontos : " + object.get("pontos"));
+                  }
+
+              }else{//Erro ao listar
+                  Log.i("ListarDados", "Erro ao consultar objeto - " + e.getMessage());
+              }
+
+          }
+      });
 
   }
 
