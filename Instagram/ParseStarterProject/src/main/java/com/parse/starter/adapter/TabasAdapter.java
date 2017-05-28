284c0bdd138b4197587ff771bbc0b1edc282a21f
Lucas Ferreira <lukin_fs@hotmail.com>
2017-05-28 12:06:11 -0300

    Adicionar icones em abas

--- Instagram/ParseStarterProject/src/main/java/com/parse/starter/adapter/TabasAdapter.java
+++ Instagram/ParseStarterProject/src/main/java/com/parse/starter/adapter/TabasAdapter.java
@@ -1,10 +1,16 @@
 package com.parse.starter.adapter;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.content.ContextCompat;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ImageSpan;
 
+import com.parse.starter.R;
 import com.parse.starter.fragments.HomeFragment;
 import com.parse.starter.fragments.UsuariosFragment;
 
@@ -14,11 +20,15 @@ import com.parse.starter.fragments.UsuariosFragment;
 public class TabasAdapter extends FragmentStatePagerAdapter {
 
     private Context context ;
-    private String[] abas = new String[]{"HOME"," USUARIOS" };
+    //private String[] abas = new String[]{"HOME"," USUARIOS" };
+    private int[] icones = new int[]{R.drawable.ic_action_home , R.drawable.ic_people };
+    private int tamanhoIcone;
 
     public TabasAdapter(FragmentManager fm, Context c) {
         super(fm);
         this.context = c ;
+        double escala = this.context.getResources().getDisplayMetrics().density ;
+        tamanhoIcone = (int)(36 * escala);
     }
 
     @Override
@@ -39,11 +49,22 @@ public class TabasAdapter extends FragmentStatePagerAdapter {
 
     @Override
     public CharSequence getPageTitle(int position) {
-        return abas[position];
+        //Recuperar o icone de acordo com a posição
+        Drawable drawable = ContextCompat.getDrawable(this.context, icones[position]);
+        drawable.setBounds(0,0,tamanhoIcone,tamanhoIcone);
+
+        //Permitir colocar uma imagem dentro de um texto
+        ImageSpan imageSpan = new ImageSpan(drawable);
+
+        //Classe utilizada para retornar um CharSequence
+        SpannableString spannableString = new SpannableString(" ");
+        spannableString.setSpan(imageSpan, 0, spannableString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        return spannableString;
     }
 
     @Override
     public int getCount() {
-        return abas.length;
+        return icones.length;
     }
 }
