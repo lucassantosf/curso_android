e941fb5d61ae2cf8da2303d55e42bbfe9ca66f03
Lucas Ferreira <lukin_fs@hotmail.com>
2017-06-07 21:07:27 -0300

    Lsitagem de Postagem

---
+++ Instagram/ParseStarterProject/src/main/java/com/parse/starter/adapter/HomeAdapter.java
@@ -0,0 +1,61 @@
+package com.parse.starter.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import com.parse.ParseObject;
+import com.parse.starter.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lucas on 07/06/2017.
+ */
+public class HomeAdapter extends ArrayAdapter<ParseObject> {
+
+    private Context context;
+    private ArrayList<ParseObject> postagens;
+
+    public HomeAdapter(Context c, ArrayList<ParseObject> objects) {
+        super(c, 0, objects);
+        this.context = c;
+        this.postagens = objects;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View view = convertView;
+
+        /*
+        * Verificar se não existe o objeto view criado,
+        * pois a view utilizada é armazenada no cache do android e fica na variável converView
+        * */
+
+        if( view == null){
+
+            //Inicializa objeto para montagem do layout
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService( context.LAYOUT_INFLATER_SERVICE );
+
+            //montar a view apartir do XML
+            view = inflater.inflate(R.layout.lista_postagem, parent, false);
+
+        }
+
+        //verifica se existe postagem
+        if( postagens.size() > 0 ){
+            //recuperar componentes da tela
+            ImageView imagemPostagem = (ImageView) view.findViewById(R.id.image_lista_postagem);
+            ParseObject parseObject = postagens.get(position);
+            parseObject.getParseFile("imagem");
+        }
+
+        return view;
+
+    }
+}
--- Instagram/ParseStarterProject/src/main/java/com/parse/starter/fragments/HomeFragment.java
+++ Instagram/ParseStarterProject/src/main/java/com/parse/starter/fragments/HomeFragment.java
@@ -6,25 +6,79 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 import com.parse.starter.R;
+import com.parse.starter.adapter.HomeAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
 
+    private ListView listview;
+    private ArrayList<ParseObject> postagens;
+    private ArrayAdapter<ParseObject> adapter;
+    private ParseQuery<ParseObject> query;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+
+        //Montar a listView
+        postagens = new ArrayList<>();
+        listview = (ListView) view.findViewById(R.id.list_postagem_home);
+        adapter = new HomeAdapter( getActivity(), postagens);
+        listview.setAdapter( adapter);
+
+        //recuperar postagens
+        getPostagens();
+
+        return view;
+    }
+
+    private void getPostagens(){
+
+        //recuperar imagens das postagens
+        query = ParseQuery.getQuery("imagem");
+        query.whereEqualTo("username", ParseUser.getCurrentUser().getUsername());
+        query.orderByDescending("createdAt");
+
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+
+                if( e == null){
+
+                    if( objects.size() > 0){
+                        postagens.clear();
+                        for(ParseObject parseObject : objects){
+                            postagens.add(parseObject);
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+
+                }else{ // erro
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
 
 }
