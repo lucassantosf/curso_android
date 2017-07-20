59d38d563fb402822be5d2077c530909ab6447f2
Lucas Ferreira <lukin_fs@hotmail.com>
2017-07-20 19:53:41 -0300

    Listar postagens de usuários

--- Instagram/ParseStarterProject/build/generated/source/r/debug/com/parse/starter/R.java
+++ Instagram/ParseStarterProject/build/generated/source/r/debug/com/parse/starter/R.java
@@ -2000,12 +2000,13 @@ containing a value of this type.
         public static final int fundo_degrade=0x7f02003e;
         public static final int ic_action_cameraa=0x7f02003f;
         public static final int ic_action_home=0x7f020040;
-        public static final int ic_people=0x7f020041;
-        public static final int instagramlogo=0x7f020042;
-        public static final int logo=0x7f020043;
-        public static final int notification_template_icon_bg=0x7f020046;
-        public static final int snackbar_background=0x7f020044;
-        public static final int usuario=0x7f020045;
+        public static final int ic_keyboard_arrow_left=0x7f020041;
+        public static final int ic_people=0x7f020042;
+        public static final int instagramlogo=0x7f020043;
+        public static final int logo=0x7f020044;
+        public static final int notification_template_icon_bg=0x7f020047;
+        public static final int snackbar_background=0x7f020045;
+        public static final int usuario=0x7f020046;
     }
     public static final class id {
         public static final int action0=0x7f0c007c;
--- Instagram/ParseStarterProject/src/main/java/com/parse/starter/activity/FeedUsuariosActivity.java
+++ Instagram/ParseStarterProject/src/main/java/com/parse/starter/activity/FeedUsuariosActivity.java
@@ -7,9 +7,19 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.starter.R;
+import com.parse.starter.adapter.HomeAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FeedUsuariosActivity extends AppCompatActivity {
 
@@ -17,6 +27,8 @@ public class FeedUsuariosActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private ListView listView;
     private String userName;
+    private ArrayAdapter<ParseObject> adapter;
+    private ArrayList<ParseObject> postagens;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +43,51 @@ public class FeedUsuariosActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar_feed_usuario);
         toolbar.setTitle( userName );
         toolbar.setTitleTextColor(R.color.preto);
+        toolbar.setNavigationIcon(R.drawable.ic_keyboard_arrow_left);
+        setSupportActionBar(toolbar);
+
+        //Configurar list view e adapter
+        postagens = new ArrayList<>();
+        listView = (ListView) findViewById(R.id.toolbar_feed_usuario);
+        adapter = new HomeAdapter( getApplicationContext(), postagens);
+        listView.setAdapter( adapter);
+
+        //recuperar postagens
+        getPostagens();
+
+
+    }
+
+
+    private void getPostagens(){
+
+        //recuperar imagens das postagens
+        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("imagem");
+        query.whereEqualTo("username", userName);
+        query.orderByAscending("createdAt");
+
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+
+                if(e==null) {//sucesso
+
+                    //verifica se existe postagens
+                    if( objects.size() > 0 ){
+                        postagens.clear();
+                        for( ParseObject parseObject : objects){
+                            postagens.add(parseObject);
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+
+
+
+                }else{//erro
+                    Toast.makeText(getApplicationContext(), "Erro ao recuperar o feed!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
 
     }
