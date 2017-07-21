eea50c0a5f3b7f2af3cc32918b150f0de06f97cc
Lucas Ferreira <lukin_fs@hotmail.com>
2017-07-21 19:23:21 -0300

    Alterando logo do App

--- Instagram/ParseStarterProject/build/generated/source/r/debug/com/parse/starter/R.java
+++ Instagram/ParseStarterProject/build/generated/source/r/debug/com/parse/starter/R.java
@@ -2222,7 +2222,8 @@ containing a value of this type.
         public static final int menu_main=0x7f0d0000;
     }
     public static final class mipmap {
-        public static final int ic_launcher=0x7f030000;
+        public static final int ic_instagram=0x7f030000;
+        public static final int ic_launcher=0x7f030001;
     }
     public static final class string {
         public static final int abc_action_bar_home_description=0x7f060000;
